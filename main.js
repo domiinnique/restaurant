5e43e8985bae2e18626b55eb32ed412a0c5fa7ff
domiinnique <domiinikawozniak@gmail.com>
2020-02-07 17:06:36 +0100

    kinda works

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-fetch('https://kea-alt-del.dk/t5/api/productlist")
-    .then(function (response) {
+fetch("https://kea-alt-del.dk/t5/api/productlist")
+    .then(function(response) {
     return response.json()
 })
-      .then(function (data) {
+      .then(function (data){
     showData(data)
 })
 
       function showData(jsonData) {
+    jsonData.forEach(showDishes)
     console.log(jsonData)
+}
+
+function showDishes(dishes){
+     const template = document.querySelector("template").content;
+    const clone = template.cloneNode(true);
+
+    clone.querySelector("h3").textContent = dishes.name;
+    clone.querySelector("h3").textContent = dishes.shortdescription;
+    clone.querySelector("h4").textContent = dishes.longdescription;
+
+    const parent = document.querySelector("main");
+    parent.appendChild(clone)
 }
