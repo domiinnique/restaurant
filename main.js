007e2c83365c272257c6ac69ea533e174c9cbf35
domiinnique <domiinikawozniak@gmail.com>
2020-02-07 15:42:08 +0100

    template ok

---
+++ main.js
@@ -0,0 +1,11 @@
+fetch('https://kea-alt-del.dk/t5/api/productlist")
+    .then(function (response) {
+    return response.json()
+})
+      .then(function (data) {
+    showData(data)
+})
+
+      function showData(jsonData) {
+    console.log(jsonData)
+}
